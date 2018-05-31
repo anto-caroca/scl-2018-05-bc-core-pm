fc2713b975d130c4a922c6e9e2fe05e1f366b39f
antonella <antonella_caroca@hotmail.com>
2018-05-30 22:39:17 -0400

    agregué una función para cada archivo html que valida el botón enviar. También puse un alert.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-/* Acá va tu código */
-function btnEnviar(){
 
-    var txtMensaje = document.caesarForm.mensaje.value;
+/* HACER COMMIT */
+   function validate(){
     
-    if(txtMensaje===""){
-        alert("debe escribir un mensaje");
-        document.caesarForm.mensaje.focus();
+      let textArea1= document.getElementById("encrypt").value;
+
+      if( textArea1 == null || textArea1.length == 0 || /^\s+$/.test(textArea1) ) {
         return false;
-    }else{
-        alert("mensaje enviado");
-        return true;
+      
+        }else{
+          alert("Conozca su mensaje cifrado");
+          return true;
+        }
+        
     }
+
+    function validate2(){
+     
+      let textArea2= document.getElementById("decrypt").value;
+      if( textArea2 == null || textArea2.length == 0 || /^\s+$/.test(textArea2) ) {
+        return false;
+      
+        }else{
+          alert("Conozca su mensaje descifrado");
+          return true;
+        }
+      
     }
+
+
+    
+
+
+
