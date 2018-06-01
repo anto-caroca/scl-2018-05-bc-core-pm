ee1ccc43ce0361c21614ded737a71772dcdd5eb7
antonella <antonella_caroca@hotmail.com>
2018-06-01 10:36:27 -0400

    agregué 2 funciones para mostrar el mensaje

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 
 /* HACER COMMIT */
-   function validate(){
-    
-      let textArea1= document.getElementById("encrypt").value;
-
-      if( textArea1 == null || textArea1.length == 0 || /^\s+$/.test(textArea1) ) {
-        return false;
-      
-        }else{
-          alert("Conozca su mensaje cifrado");
-          return true;
-        }
-        
+    validate = () =>
+   {
+      let offsetNumber= parseInt(document.getElementById("offsetNumber").value);
+          let textArea2= document.getElementById("encrypt").value;
+         document.getElementById("cipheredMessage").innerHTML = window.cipher.encode(textArea2, offsetNumber);
+       
     }
 
-    function validate2(){
+     validate2 = () =>
+     {
      
-      let textArea2= document.getElementById("decrypt").value;
-      if( textArea2 == null || textArea2.length == 0 || /^\s+$/.test(textArea2) ) {
-        return false;
-      
-        }else{
-          alert("Conozca su mensaje descifrado");
-          return true;
-        }
-      
+      let decipher= document.getElementById("offsetNumber").value;
+      let textArea2= document.getElementById("encrypt").value;
+      document.getElementById("cipheredMessage").innerHTML = window.cipher.decode(textArea2, decipher);
+
+     }
+     
+
+/* no funciona
+
+  cipher.createCipherWithOffset = (offset) => {
+
+    encode:(offset)=> {
+       let offsetNumber= parseInt(document.getElementById("offsetNumber").value);
+          let textArea2= document.getElementById("encrypt").value;
+         document.getElementById("cipheredMessage").innerHTML = window.cipher.encode(textArea2, offsetNumber);
+    },
+
+    decode:(offset)=> {
+       
     }
 
+}*/
 
-    
 
 
 
