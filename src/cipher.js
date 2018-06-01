f29f173daacdefc42d17c1f9e4bbb01f8771b5b9
antonella <antonella_caroca@hotmail.com>
2018-06-01 15:28:00 -0400

    cambié formula para que decifre en mayusculas

--- src/cipher.js
+++ src/cipher.js
@@ -32,8 +32,29 @@ return result;
 
   decode: (word, offset) => {
   
-    let result=window.cipher.encode(word, offset);
-     return result;
+    let result = "";
+    
+    //recorre cada letra del texto
+    for (let letter = 0; letter < word.length; letter++) {
+        
+         //obtener codigo ascii 
+        let ascii = word.charCodeAt(letter);
+
+        // codigo para mayusculas es6 cifrado cesar
+        if(ascii >= 65 && ascii <=  90) {
+           result += String.fromCharCode((ascii + 65 - offset) % 26 + 65).toUpperCase(); 
+        
+        // codigo ascii para minusculas
+        }else if(ascii >= 97 && ascii <= 122){
+            result += String.fromCharCode((ascii + 97 - offset) % 26 + 97).toLowerCase();
+            
+        // deja pasar si no es letra
+        }else {
+            result += word.charAt(letter);      
+        }
+      }
+
+return result;
     }
  
   
