aa614e2e80b9faa961e30679e6d357a8624369b0
antonella <antonella_caroca@hotmail.com>
2018-06-01 10:29:02 -0400

    arreglé función que codifica el mensaje

--- src/cipher.js
+++ src/cipher.js
@@ -1,61 +1,44 @@
+//import { STATUS_CODES } from "http";
+
 window.cipher = {
-/* HACER COMMIT*/
-  encode: (offset, string) => {
-    let offset=0;
-    let string=" ";
-    let result =" ";
- 
+
+  encode: (word, offset) => {
+    
+    let result = "";
+    
     //recorre cada letra del texto
-    for (let letter = 0; letter < string.length; letter++) {
-          
+    for (let letter = 0; letter < word.length; letter++) {
+        
          //obtener codigo ascii 
-        let ascii = string.charCodeAt(letter);
+        let ascii = word.charCodeAt(letter);
 
-        // codigo para mayusculas
+        // codigo para mayusculas es6 cifrado cesar
         if(ascii >= 65 && ascii <=  90) {
-           result += String.fromCharCode((ascii - 65 + offset) % 26 + 65); 
-
+           result += String.fromCharCode((ascii - 65 + offset) % 26 + 65).toUpperCase(); 
+        
         // codigo ascii para minusculas
         }else if(ascii >= 97 && ascii <= 122){
-            result += String.fromCharCode((ascii - 97 + offset) % 26 + 97);
-
+            result += String.fromCharCode((ascii - 97 + offset) % 26 + 97).toLowerCase();
+            
         // deja pasar si no es letra
         }else {
-            result += string.charAt(letter);
+            result += word.charAt(letter);      
         }
-    }
+      }
 
 return result;
 
-/* 
-cipher.encode(offset, string): offset es el número de posiciones que
- queremos mover a la derecha en el alfabeto y string el mensaje (texto) 
- que queremos cifrar.
-
-[A-Za-z]    regex
-
-(x - 65 + n) % 26 + 65 mayusculas
-(x- 97 + n) % 26 + 97 minusculas
-
-x = numero de la letra ASCII  
-n = valor fijo
-
-var string = 'ABC';
-string.CharCodeAt(0) devuelve 65
-
-*/
-
   },
 
-  decode: (string, offset) => {
-    //String.fromCharCode(72); devuelve "H" 
-    
-    let result = " ";
-    let string = (26-offset)%26;
-    result= encode;
-    return result;
-  }
-} 
+  decode: (word, offset) => {
+  
+    let result=window.cipher.encode(word, offset);
+     return result;
+    }
+ 
+  
+}
+
 
 
   
